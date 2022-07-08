ae005b7bad3c5d84a67a3da2815cd5d7c78bf624
jishan <you@example.com>
2022-07-08 10:29:54 +0530

    Updated task

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,48 +1,48 @@
-import 'dart:math';
+// import 'dart:math';
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-Color a = Color(Random().nextInt(0xffffffff));
-Color b = Color(Random().nextInt(0xffffffff));
+// Color a = Color(Random().nextInt(0xffffffff));
+// Color b = Color(Random().nextInt(0xffffffff));
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Container colour swapping"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            var temp = a;
-            a = b;
-            b = temp;
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Container colour swapping"),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           setState(() {
+//             var temp = a;
+//             a = b;
+//             b = temp;
            
-          });
-        },
-        child: Icon(Icons.swap_horiz,size: 28,),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Wrap(
-          children: [
-            Container(height: 200, width: 200, color: b),
-            Container(
-              height: 200,
-              width: 200,
-              color: a,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//           });
+//         },
+//         child: Icon(Icons.swap_horiz,size: 28,),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Wrap(
+//           children: [
+//             Container(height: 200, width: 200, color: b),
+//             Container(
+//               height: 200,
+//               width: 200,
+//               color: a,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:internshiptask/home_page.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -7,26 +7,100 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Assignment',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const HomeScreen(),
     );
   }
 }
 
+class StatefulColorfulTile extends StatefulWidget {
+  const StatefulColorfulTile({Key? key, this.myColor}) : super(key: key);
+  
+  final myColor;
+
+  @override
+  State<StatefulColorfulTile> createState() => _StatefulColorfulTileState();
+}
+
+class _StatefulColorfulTileState extends State<StatefulColorfulTile> {
+  Color? myColor;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: widget.myColor,
+      child: const Padding(
+        padding: EdgeInsets.all(70.0),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  
+  Color? myColor;
+  
+  List<Widget> containers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    myColor = Color(
+      (math.Random().nextDouble() * 0xFFFFFF).toInt(),
+    ).withOpacity(
+      1.0,
+    );
+    containers.insert(0,  Padding(
+      padding: EdgeInsets.all(10),
+      child: StatefulColorfulTile(myColor: myColor),
+    ));
+    myColor = Color(
+      (math.Random().nextDouble() * 0xFFFFFF).toInt(),
+    ).withOpacity(
+      1.0,
+    );
+    containers.insert(1,  Padding(
+      padding: EdgeInsets.all(10),
+      child: StatefulColorfulTile(myColor: myColor),
+    ));
+  }
+
+  void swapColour() {
+    setState(() {
+      containers.insert(1, containers.removeAt(0));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: containers,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => swapColour(),
+        child: const Icon(Icons.change_circle_outlined),
+      ),
+    );
+  }
+}
